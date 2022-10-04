33fea4ee617f7483d4cedbf0c04717edf2622436
Amala02 <shwetahamala@gmail.com>
2022-10-04 13:21:57 +0530

    commit2

--- src/fluid.js
+++ src/fluid.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
  
-import FluidAnimation from 'react-fluid-animation'
+import fluidAnimation from 'react-fluid-animation/dist/fluid-animation';
+
  
 export default class App extends Component {
   render () {
     return (
-      <FluidAnimation
+      <fluidAnimation
         style={{ height: '100vh' }}
       />
     )
