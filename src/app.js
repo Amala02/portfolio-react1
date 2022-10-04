da7b02e20319968ffc893b6ca55265f892c316c5
Amala02 <shwetahamala@gmail.com>
2022-10-04 15:39:07 +0530

    commit2

--- src/app.js
+++ src/app.js
@@ -9,6 +9,24 @@ function App(){
         </div>
 
     );
+}*/
+
+import FluidAnimation from 'C:\Users\shwetah\portfolio-react\node_modules\react-fluid-animation';
+const defaultConfig = {
+    textureDownsample: 1,
+    densityDissipation: 0.98,
+    velocityDissipation: 0.99,
+    pressureDissipation: 0.8,
+    pressureIterations: 25,
+    curl: 30,
+    splatRadius: 0.005
+  }
+ 
+export default class App extends Component {
+    state = {
+        config: {
+          ...defaultConfig
+        }
 }
 
 export default App;
